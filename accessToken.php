24f3fad9c7c665c1691eba6b79475e518b1fa3e5
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-08-30 13:45:37 +0300

    access token

---
+++ accessToken.php
@@ -0,0 +1,17 @@
+<?php
+
+$consumerKey = getenv("CONSUMER_KEY");
+$consumerSecret = getenv("CONSUMER_SECRET");
+
+$access_token_url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+$headers = ['Content-Type: application/json; charset=utf-8'];
+$curl = curl_init($access_token_url);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_USERPWD, $consumerKey . ':' . $consumerSecret);
+$result = curl_exec($curl);
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+$result = json_decode($result);
+echo $access_token = $result->access_token;
+curl_close($curl);
