8efd68f6987019aa5f6da7f3d1754d28c49093cf
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-08-31 21:05:57 +0300

    transaction status

--- b2c/b2c.php
+++ b2c/b2c.php
@@ -36,3 +36,11 @@ $curl_post_data = array(
     'ResultURL' => $ResultURL,
     'Occasion' => $Occasion
 );
+
+$data_string = json_encode($curl_post_data);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt( $curl, CURLOPT_POSTFIELDS, $data_string);
+$curl_response = curl_exec($curl);
+
+echo $curl_response;
